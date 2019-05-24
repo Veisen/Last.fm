57909321d5cac8fded6bfbb2fc1bdd774dbff4e5
Veisen <aveisen@seznam.cz>
2019-05-25 01:31:36 +0200

    emby port for jellyfin

---
+++ Jellyfin.Plugin.Lastfm/Models/Requests/ScrobbleRequest.cs
@@ -0,0 +1,30 @@
+﻿namespace Jellyfin.Plugin.Lastfm.Models.Requests
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class ScrobbleRequest : BaseAuthedRequest
+    {
+        public string Track     { get; set; }
+        public string Album     { get; set; }
+        public string Artist    { get; set; }
+        public int    Timestamp { get; set; }
+        public string MbId      { get; set; }
+
+        public override Dictionary<string, string> ToDictionary()
+        {
+            var dict = new Dictionary<string, string>(base.ToDictionary()) 
+            {
+                { "track",     Track },
+                { "artist",    Artist },
+                { "timestamp", Timestamp.ToString() },
+            };
+            if (!string.IsNullOrWhiteSpace(Album))
+                dict.Add("album", Album);
+            if (!string.IsNullOrWhiteSpace(MbId))
+                dict.Add("mbid", MbId);
+            return dict;
+        }
+    }
+}
---
+++ Jellyfin.Plugin.Lastfm/RestApi.cs
@@ -0,0 +1,32 @@
+﻿namespace Jellyfin.Plugin.Lastfm
+{
+    using Api;
+    using MediaBrowser.Common.Net;
+    using MediaBrowser.Model.Serialization;
+    using MediaBrowser.Model.Services;
+    using Microsoft.Extensions.Logging;
+
+    [Route("/Lastfm/Login", "POST")]
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RestApi : IService
+    {
+        private readonly LastfmApiClient _apiClient;
+        private readonly ILogger _logger;
+
+        public RestApi(IJsonSerializer jsonSerializer, IHttpClient httpClient, ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("AutoOrganize");
+            _apiClient = new LastfmApiClient(httpClient, jsonSerializer, _logger);
+        }
+
+        public object Post(Login request)
+        {
+            return _apiClient.RequestSession(request.Username, request.Password).Result;
+        }
+    }
+}
